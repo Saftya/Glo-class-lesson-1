4475335296309cc7eea51674f18cdea62b2acf5f
Samal K <safty@mail.ru>
2023-06-24 16:03:14 +0600

    введены новые переменные, создан alert и вывод в консоль

---
+++ script.js
@@ -0,0 +1,10 @@
+let user = prompt("What is your name?");
+alert("Welcome dear " + user + "!");
+console.log("How are you?");
+
+const title = 1
+const screen = 2
+const screenPrice = 3
+const rollback = 4
+const fullPrice = 5
+const adaptive = 6
